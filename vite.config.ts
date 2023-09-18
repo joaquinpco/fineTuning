2e07b9e6e7bd2063cec6aaf8bf6cadc26210fb60
joaquinpco <joaquin1208@me.com>
2023-09-18 22:27:22 +0200

    Added pluggins

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   build: {
     target: 'esnext', // you can also use 'es2020' here
   },
